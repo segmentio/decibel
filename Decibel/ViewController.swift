f33f02e05b6fa7e2781471e481ea08dbb40fe68f
Saamer Mansoor <i@saamer.me>
2022-01-16 10:52:01 -0500

    Update ViewController.swift

--- Decibel/ViewController.swift
+++ Decibel/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  Decibel
 //
-//  Created by Saamer Mansoor on 1/16/22.
+//  Created by Peter Reinhardt on 8/13/16.
+//  Copyright © 2016 Peter Reinhardt. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+         super.didReceiveMemoryWarning()
+         // Dispose of any resources that can be recreated.
+    }
 }
 
