a15079e02d11fbe4f4c56c0fa4795e074ea81762
Saamer Mansoor <i@saamer.me>
2022-01-16 09:30:06 -0500

    Moved old project to src, created blank project from template

---
+++ Decibel/Decibel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Decibel
+//
+//  Created by Saamer Mansoor on 1/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
